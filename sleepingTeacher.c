ab56cb9544f6236a6bd611cff9f1d399a6ec6ec9
snjumaheshwari <33566000+snjumaheshwari@users.noreply.github.com>
2018-04-12 22:50:20 +0530

    Update sleepingTeacher.c

--- sleepingTeacher.c
+++ sleepingTeacher.c
@@ -1,60 +1,61 @@
-//for one barbar and many custormer
-//it is very much like producer consumer problem
 #include<stdio.h>
 #include<semaphore.h>
 #include<pthread.h>
-sem_t barber,customer,waitingroom; //waiting room is mutex for updating the seats
-int seats=4;  //no. of free seats in waiting room
-void *barbercutting()
+sem_t teacher,student,waitingroom; //waiting room is mutex for updating the seats
+int seats=3;  //no. of free seats in waiting room
+void *teacherteaching()
 {
 	do
 	{
-		sem_wait(&customer); //decrease customer
+		sem_wait(&student); //decrease student
 		sem_wait(&waitingroom); 
 		
 		seats+=1;	//increase the no. of seating in waiting room
-		printf("barber is cutting hair\n");
-		sleep(1);	//let barbar take 1 sec to cut hair
+		printf("teacher is teaching the student\n");
+		sleep(1);	//let teacher take 1 sec to resolve query
 		
-		sem_post(&waitingroom);	  
-		sem_wait(&barber);  //after cutting hair barber sleeps
+		sem_post(&waitingroom);
+		printf("teacher is going back to sleep as no students came\n");
+		sem_wait(&teacher);  //after resolving query teacher sleeps
 	}while(1);
 }
 
-void *customercoming()
+void *studentcoming()
 {
 	sem_wait(&waitingroom);
 	if(seats>0)
 	{
+		printf("student has come\n");
 		seats=seats-1;
-		sem_post(&customer);
+		sem_post(&student);
 		sem_post(&waitingroom);
-		printf("customer in waiting room\n");
-		sem_post(&barber);
+		printf("student in waiting room\n");
+		sem_post(&teacher);
 	}
 	else //for no seats available
 	{
 		sem_post(&waitingroom);
-		printf("no free seat\n");
+		printf("student came but no free seats in waiting room\n");
 	}
 }
 
 int main()
 {
-	pthread_t bar,cus[100];	
-	sem_init(&barber,0,1);
+	pthread_t tea,stu[100];	
+	sem_init(&teacher,0,1);
 	sem_init(&waitingroom,0,1);
-	sem_init(&customer,0,0);
+	sem_init(&student,0,0);
 
-	sem_wait(&barber);//to make sem 0, to make barber sleep initially
+	printf("Teacher sleeping\n");
+	sem_wait(&teacher);//to make sem 0, to make teacher sleep initially
 	
-	pthread_create(&bar,NULL,barbercutting,NULL);
+	pthread_create(&tea,NULL,teacherteaching,NULL);
 	for(int i=0;i<100;i++)
-		pthread_create(&cus[i],NULL,customercoming,NULL);
+		pthread_create(&stu[i],NULL,studentcoming,NULL);
 //	sleep(1);
 	
 	
 	for(int i=0;i<100;i++)
-		pthread_join(cus[i],NULL);
-	pthread_join(bar,NULL);
+		pthread_join(stu[i],NULL);
+	pthread_join(tea,NULL);
 }
